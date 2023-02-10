829a67f4342ce066e36f61b6e009ce14c5c1cf70
dukelester <dukelester4@gmail.com>
2023-02-10 17:20:21 +0300

    Schema
    created the table sql for users, blogs, products and contact us tables to hold information.

---
+++ newspress/schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS blog;
+DROP TABLE IF EXISTS contact;
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    phone TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    fullname TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE blog (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    category TEXT NOT NULL,
+    photo BLOB,
+    video_url TEXT,
+    tags TEXT,
+    FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE contact (
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    comment TEXT NOT NULL,
+);
+
+CREATE TABLE products (
+    title TEXT NOT NULL,
+    price DECIMAL NOT NULL,
+    description TEXT NOT NULL,
+    seller_id INTEGER NOT NULL,
+    photo BLOB
+    category TEXT NOT NULL,
+    additional_info TEXT NOT NULL,
+    review TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (seller_id) REFERENCES user (id)
+);
