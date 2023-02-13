9cc3c839c0ec2c2baf24d72d65e04663fa40444d
dukelester <dukelester4@gmail.com>
2023-02-13 23:39:35 +0300

    Login
    user can login with username and password. If the username and password are incorrect, you get an error.

--- newspress/auth.py
+++ newspress/auth.py
@@ -43,3 +43,25 @@ def user_registration():
                 return redirect(url_for('auth.login'))
         flash(error)
     return render_template('register.html')
+
+@blueprint.route('/login', methods=['GET', 'POST'])
+def user_login():
+    ''' The user login view '''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_database()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username),
+        ).fetchone()
+        if user is None:
+            error = ' Invalid username'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password'
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        flash(error)
+    return render_template('login.html')
