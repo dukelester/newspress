bc98f3f0d5332ef50a2f00b7bffa0568af548f02
dukelester <dukelester4@gmail.com>
2023-02-14 11:53:28 +0300

    All the posts
    fetch all the posts from the database . This will list all the posts from the table of posts

--- newspress/blog.py
+++ newspress/blog.py
@@ -11,4 +11,12 @@ blueprint = Blueprint('blog', __name__)
 @blueprint.route('/')
 def index():
     ''' The index route'''
+    db = get_database()
+    blog_posts = db.execute(
+        ''' SELECT p.id, title, body, category, created_at, photo, video_url, tags
+            FROM blog p JOIN user u ON p.author_id = u.id
+            ORDER BY created_at DESC
+        '''
+    ).fetchall()
+    print(blog_posts)
     return render_template('index.html')
