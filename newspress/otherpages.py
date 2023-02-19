8985dbe8c878f6d94293a233153196f3c5459f6b
dukelester <dukelester4@gmail.com>
2023-02-19 11:42:09 +0300

    Implemented the contact us page...
    users can send a comment to us and including their details.

--- newspress/otherpages.py
+++ newspress/otherpages.py
@@ -1,6 +1,8 @@
 from flask import (
-    request, render_template, g, current_app, Blueprint, redirect, url_for
+    request, render_template, g, current_app, Blueprint, redirect, url_for, flash
 )
+
+from newspress.database import get_database
 
 blueprint = Blueprint('contact_us', __name__)
 
@@ -8,5 +10,38 @@ blueprint = Blueprint('contact_us', __name__)
 def contact_us():
     ''' Contact us'''
     if request.method == 'POST':
-        pass
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        subject = request.form['subject']
+        comment = request.form['comment']
+
+        db = get_database()
+        error = None
+
+        if not first_name:
+            error = 'First name is required'
+        elif not last_name:
+            error = 'Last name is required'
+        elif not email:
+            error = 'Email is required'
+        elif not phone:
+            error = 'Phone number is required'
+        elif not subject:
+            error = 'Subject is required'
+        elif not comment:
+            error = 'The comment is required'
+        if error is not None:
+            flash(error)
+        else:
+            db.execute(
+                ''' INSERT INTO contact (first_name, last_name, email, phone, subject, comment)
+                VALUES (?, ?, ?, ?, ?, ?)
+                ''',
+                (first_name, last_name, email, phone, subject, comment)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
     return render_template('contact.html')
