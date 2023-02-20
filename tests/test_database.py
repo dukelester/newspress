8eb26a81150d35940d4c4c931a6ba17d924e56aa
dukelester <dukelester4@gmail.com>
2023-02-20 22:56:09 +0300

    This test uses Pytest’s monkeypatch fixture to replace the init_db function with one that records that it’s been called. The runner fixture you wrote above is used to call the init-db command by name.

--- tests/test_database.py
+++ tests/test_database.py
@@ -10,3 +10,13 @@ def test_get_close_db(app):
     with pytest.raises(sqlite3.ProgrammingError) as e:
         db.execute('SELECT 1')
     assert 'closed' in str(e.value)
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+    def fake_init_db():
+        Recorder.called = True
+    monkeypatch.setattr('newspress.database.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Database initialized successfully!' in result.output
+    assert Recorder.called
