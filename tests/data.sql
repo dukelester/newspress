a25ec382ea1ec34fef0109ab28eb7d9fa34f174e
dukelester <dukelester4@gmail.com>
2023-02-20 20:35:53 +0300

    created test users to be inserted into the database.

---
+++ tests/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO user (username, phone, email, fullname, password)
+VALUES ('testuser', '0756780034', 'testuser@gmail.com', 'test user',
+'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'
+),
+('otheruser', '073456789', 'otheruser@gmaul.com', 'other user',
+'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79')
+
