5cbf905ca9b871aa714115283911ea9158aa5072
dukelester <dukelester4@gmail.com>
2023-02-20 20:58:47 +0300

    Added testing products to the database

--- tests/data.sql
+++ tests/data.sql
@@ -6,9 +6,13 @@ VALUES ('testuser', '0756780034', 'testuser@gmail.com', 'test user',
 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79')
 
 INSERT INTO blog (author_id, title, body, category, photo, video_url, tags) VALUES
-(1, 'test blog', 'a test blog post for testing', 'testing', 'https://test.com/test',
+(1, 'test blog', 'a test blog post for testing', 'testing', 'testing/photo.png','https://test.com/test',
 'test, testing, user test, test blog'),
-(1, 'test blog 2', 'a test blog 2 post for testing', 'testing', 'https://test.com/test',
+(1, 'test blog 2', 'a test blog 2 post for testing', 'testing', 'testing/photo.png','https://test.com/test',
 'test, testing, user test, test blog'),
-(2, 'test blog 3', 'a test blog post 3 for testing', 'testing', 'https://test.com/test',
+(2, 'test blog 3', 'a test blog post 3 for testing', 'testing', 'testing/photo.png','https://test.com/test',
 'test, testing, user test, test blog'),
+
+INSERT INTO products VALUES (title, price, detailed_description, photo, seller_id) VALUES
+('testing products', '2000', ' good testing product to test', 'testing', 'test, testing, additional test', 'testing/photo.png', 1),
+('testing product 2', '2700', ' good testing product to test', 'testing', 'test, testing, additional test', 'testing/photo.png', 3),
