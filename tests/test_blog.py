7203f73593e02acbb0f7d0203ebfb9215c3a6e86
dukelester <dukelester4@gmail.com>
2023-02-24 12:44:07 +0300

    test creating a new blog
    post a new blog post into the database

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -14,3 +14,16 @@ def test_index(client, auth):
 def test_exists_required(client, auth, path):
     auth.login()
     assert client.post(path).status_code == 404
+
+def test_create_blog(client, auth, app):
+    ''' creating new post '''
+    auth.login()
+    assert client.get('/create').status_code == 200
+    client.post('/create', data={
+        'title': 'test blogger', 'body': '',
+    })
+
+    with app.app_context():
+        db = get_database()
+        count = db.execute('SELECT COUNT(id) FROM blog').fetchone()[0]
+        assert count == 4
