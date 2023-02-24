ec7f23558a3470219de90c786a244bdb2681b869
dukelester <dukelester4@gmail.com>
2023-02-24 12:18:04 +0300

    Test the user registration
    allow users to register the display the error messages if any.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,6 @@ from flask import g, session
 
 from newspress.database import get_database
 
-
 def test_register(client, app):
     ''' Test the user register process '''
     assert client.get('/auth/register').status_code == 200
@@ -18,15 +17,21 @@ def test_register(client, app):
     assert response.headers['Location'] == '/auth/login'
     with app.app_context():
         assert get_database().execute(
-            ''' SELECT * FROM user WHERE username = 'duketester''',
+            ''' SELECT * FROM user WHERE username = 'duketester' ''',
         ).fetchone() is not None
 
+
 @pytest.mark.parametrize(('username', 'phone', 'email', 'fullname', 'password'),
-   (('', '073458922', 'duketest@gmail.com',  'duke tester', 'duketest2030',  b'Username is required'),
-    ('duketester', '073458922', 'duketest@gmail.com',  'duke tester', '', b'Password is required'),
-    ('duketester', '', 'duketest@gmail.com',  'duke tester', 'duketest2030', b'Phone number is required'),
-    ('duketester', '073458922', '',  'duke tester', 'duketest2030', b'Email is required'),
-    ('duketester', '073458922', 'duketest@gmail.com',  '', 'duketest2030', b'Full Name is required'),
+   (('', '073458922', 'duketest@gmail.com',  'duke tester',
+    'duketest2030',  b'Username is required'),
+    ('duketester', '073458922', 'duketest@gmail.com',
+    'duke tester', '', b'Password is required'),
+    ('duketester', '', 'duketest@gmail.com',  'duke tester',
+    'duketest2030', b'Phone number is required'),
+    ('duketester', '073458922', '',  'duke tester',
+    'duketest2030', b'Email is required'),
+    ('duketester', '073458922', 'duketest@gmail.com',  '',
+    'duketest2030', b'Full Name is required'),
     ))
 def test_register_validate_input(client, username, phone, email, fullname, password, message):
     response = client.post(
